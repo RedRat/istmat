68286da22f06346cb84ec5cb502dafde1b4de221
Alexey Markov <redrat@mail.ru>
2013-09-28 21:49:14 +0400

    Add images, page template and theme settings

---
+++ template.php
@@ -0,0 +1,95 @@
+<?php
+
+// Auto-rebuild the theme registry during theme development.
+if (theme_get_setting('istmat_autorebuild') && !defined('MAINTENANCE_MODE')) {
+  system_rebuild_theme_data();
+  drupal_theme_rebuild();
+}
+
+function istmat_preprocess_maintenance_page(&$vars) {
+//  istmat_preprocess_html($vars);
+  istmat_preprocess_page($vars);
+}
+
+/*
+// When https://drupal.org/node/865536 will be solved, this code may be
+// converted to the drupal_add_js() call.
+function istmat_preprocess_html(&$vars) {
+  if (theme_get_setting('istmat_respondjs')) {
+    $respondjs = array(
+      '#tag' => 'script',
+      '#attributes' => array(
+        'type'=> 'text/javascript',
+        'src' => file_create_url(drupal_get_path('theme', 'istmat').'/js/respond.min.js'),
+      ),
+//      '#browsers' => array('IE' => 'lt IE 9', '!IE' => FALSE),
+//      '#pre_render' => array('drupal_pre_render_conditional_comments'),
+      '#weight' => 999,
+      '#value' => '',
+      '#prefix' => '<!--[if lte IE 8]>'.PHP_EOL,
+      '#suffix' => '<![endif]-->'.PHP_EOL,
+    );
+    drupal_add_html_head($respondjs, 'respondjs');
+  }
+}*/
+
+function istmat_preprocess_page(&$vars) {
+//  $search_form = drupal_get_form('search_form');
+//  $vars['search_form'] = drupal_render($search_form);
+  $block = module_invoke('search','block_view','search');
+  $vars['search_form'] = render($block);
+
+//  $vars['logotype'] = file_create_url(drupal_get_path('theme', 'istmat').'/images/logotype.png');
+//  $vars['header_links'] = ($vars['logged_in']) ? '<a href="'.$vars['base_path'].'user/'.$vars['user']->uid.'">'.t('Profile').'</a> / <a href="'.$vars['base_path'].'user/logout">'.t('Logout').'</a>' : '<a href="'.$vars['base_path'].'user" rel="nofollow">'.t('Login').'</a> / <a href="'.$vars['base_path'].'user/register" rel="nofollow">'.t('Register').'</a>';
+//  $vars['header_icons'] = '<a href="'.$vars['base_path'].'rss.xml" title="Подписка на ленту новостей"></a><a href="'.$vars['base_path'].'#" title="Добавить сайт в закладки"></a><a href="'.$vars['base_path'].'#" title="Карта сайта"></a><a href="mailto://'.variable_get('site_mail', ini_get('sendmail_from')).'" title="Отправить нам письмо"></a>';
+
+//  dsm($vars);
+//  dsm($search_form);
+//  dsm($block);
+  if (theme_get_setting('istmat_respondjs')) {
+    drupal_add_js(
+      drupal_get_path('theme', 'istmat').'/js/respond.min.js',
+      array(
+        'group' => JS_LIBRARY,
+        'every_page' => TRUE,
+        'weight' => -999,
+        'preprocess' => FALSE,
+      )
+    );
+  }
+}
+
+/*
+function istmat_field__field_area($vars) {
+  dsm($vars);
+}
+*/
+
+function istmat_form_alter(&$form, &$form_state, $form_id) {
+  // Id's of forms that should be ignored
+  $form_ids = array(
+    'node_form',
+    'system_site_information_settings',
+    'user_profile_form',
+  );
+
+  // Only wrap in container for certain form
+  if (isset($form['#form_id']) && !in_array($form['#form_id'], $form_ids) && !isset($form['#node_edit_form'])) {
+    $form['actions']['#theme_wrappers'] = array();
+  }
+
+  if ($form_id == 'search_block_form') {
+    $form['search_block_form']['#size'] = 35;
+    $form['search_block_form']['#class'] = 'search-form';
+    $form['search_block_form']['#attributes']['title'] = t('Введите ключевые слова для поиска');
+    $form['actions']['submit'] = array('#type' => 'image_button', '#src' => drupal_get_path('theme', 'istmat').'/images/head_search.png');
+/*
+    $form['search_block_form']['#title'] = t('Search'); // Change the text on the label element
+    $form['search_block_form']['#title_display'] = 'invisible'; // Toggle label visibilty
+    $form['search_block_form']['#attributes'] = array('placeholder' => t('Search'));
+    $form['actions']['submit']['#value'] = t('GO!'); // Change the text on the submit button
+    $form['actions']['submit'] = array('#type' => 'image_button', '#src' => base_path() . path_to_theme() . '/images/searchbutton.png');
+*/
+  }
+//  dsm($form);
+}
---
+++ theme-settings.php
@@ -0,0 +1,21 @@
+<?php
+
+function istmat_form_system_theme_settings_alter(&$form, $form_state) {
+  $form['devel'] = array(
+    '#type'          => 'fieldset',
+    '#title'         => t('Theme development settings'),
+  );
+  $form['devel']['istmat_autorebuild'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Rebuild theme registry on every page.'),
+    '#default_value' => theme_get_setting('istmat_autorebuild'),
+    '#description'   => t('During theme development, it can be very useful to continuously <a href="!link">rebuild the theme registry</a>. WARNING: this is a huge performance penalty and must be turned off on production websites.', array('!link' => 'https://drupal.org/node/173880#theme-registry')),
+  );
+  $form['devel']['istmat_respondjs'] = array(
+    '#type'          => 'checkbox',
+    '#title'         => t('Add Respond.JS for IE8 and less.'),
+    '#default_value' => theme_get_setting('istmat_respondjs'),
+    '#description'   => t('This option let you add a <a href="!link">Respond</a> script to support Media-Queries in IE8 and less. WARNING! You MUST enable CSS aggregation for this option to work.', array('!link' => 'https://github.com/scottjehl/Respond')),
+#    '#disabled'      => !variable_get('preprocess_css', 0);
+  );
+}
